89fbb4a4d4f031213cc8d72de85741688d50a74f
Yazmin Perez <zoeperez333@gmail.com>
2022-04-17 16:40:43 -0700

    tests 2-7 passing

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,10 +1,17 @@
 require('pry')
 
+# -----simplified version-----
+
 class RPS
+
   def wins?(player_one, player_two)
 
-  if player_one == 'rock' && player_two == 'scissors'
+  if player_one == 'rock' && player_two == 'scissors' || player_one == 'scissors' && player_two == 'paper' || player_one == 'paper' && player_two == 'rock'
     true
+  elsif player_one == "scissors" && player_two == "rock" || player_one == "paper" && player_two == "scissors" || player_one == "rock" && player_two == "paper"
+      false
+  elsif player_one == player_two
+      nil
     end
   end
 end
@@ -15,30 +22,62 @@ end
 
 
 
-# random: rock || paper || scissors
+#------------------- score version -------------------
 
-
-# def initialize(player_one, player_two)
-#   # hand = ['rock', 'paper', 'scissors']
-#   @player_one = player_one
-#   @player_two = player_two
+# class RPS
+# def initialize()
+#   hand = ['rock', 'paper', 'scissors']
+#   # @player_one = player_one
+#   # @player_two = player_two
 #   # @player_one = 'rock' 
 #   # @player_two = 'scissors' 
 #   # @player_one = hand.sample
 #   # @player_two = hand.sample
+#   @player_one_score = 0
+#   @player_two_score = 0
 # end
 
-# def wins?(player_one, player_two)
+# # reader method
+# def player_one_score
+#   @player_one_score
+# end
+
+# def player_two_score
+#   @player_two_score
+# end
+
+
+# # writer method
+# def player_one_score=(score)
+#   @player_one_score += score
+# end
+
+# def player_two_score=(score)
+#   @player_two_score += score
+# end
+
+# def wins?(player_one_decision, player_two_decision)
 # # hand = ['rock', 'paper', 'scissors']
 # # @player_one = player_one
 # # @player_two = player_two
 # # player_one_score = 0
 # # player_two_score = 0
 
-# if self.player_one == 'rock' && self.player_two == 'scissors'
-#   true
+# if player_one_decision == 'rock' && player_two_decision == 'scissors'
+#   @player_one_score = 1
 #   end
 # end
+# end
+# end
 
-# player = RPS.new('rock', 'scissors')
-# puts "Player-one: #{player.player_one}, Player-two: #{player.player_two}"
+
+
+
+# --------------------- notes  ---------------------
+
+  # random: rock || paper || scissors
+
+  # to add:
+  # Method player wins or loses
+  # The player score = 5 game over
+
