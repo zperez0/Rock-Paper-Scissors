35f187b76bedc16176848941b9f0ba70b60cac70
Yazmin Perez <zoeperez333@gmail.com>
2022-04-17 17:04:07 -0700

    score version: test 1 passing

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -2,19 +2,21 @@ require('pry')
 
 # -----simplified version-----
 
-class RPS
+# class RPS
 
-  def wins?(player_one, player_two)
+#   def wins?(player_one, player_two)
 
-  if player_one == 'rock' && player_two == 'scissors' || player_one == 'scissors' && player_two == 'paper' || player_one == 'paper' && player_two == 'rock'
-    true
-  elsif player_one == "scissors" && player_two == "rock" || player_one == "paper" && player_two == "scissors" || player_one == "rock" && player_two == "paper"
-      false
-  elsif player_one == player_two
-      nil
-    end
-  end
-end
+#   if player_one == 'rock' && player_two == 'scissors' || player_one == 'scissors' && player_two == 'paper' || player_one == 'paper' && player_two == 'rock'
+#     true
+
+#   elsif player_one == "scissors" && player_two == "rock" || player_one == "paper" && player_two == "scissors" || player_one == "rock" && player_two == "paper"
+#       false
+      
+#   elsif player_one == player_two
+#       nil
+#     end
+#   end
+# end
 
 
 
@@ -24,51 +26,51 @@ end
 
 #------------------- score version -------------------
 
-# class RPS
-# def initialize()
-#   hand = ['rock', 'paper', 'scissors']
-#   # @player_one = player_one
-#   # @player_two = player_two
-#   # @player_one = 'rock' 
-#   # @player_two = 'scissors' 
-#   # @player_one = hand.sample
-#   # @player_two = hand.sample
-#   @player_one_score = 0
-#   @player_two_score = 0
-# end
+class RPS
+def initialize()
+  hand = ['rock', 'paper', 'scissors']
+  # @player_one = player_one
+  # @player_two = player_two
+  # @player_one = 'rock' 
+  # @player_two = 'scissors' 
+  # @player_one = hand.sample
+  # @player_two = hand.sample
+  @player_one_score = 0
+  @player_two_score = 0
+end
 
-# # reader method
-# def player_one_score
-#   @player_one_score
-# end
+# reader method
+def player_one_score
+  @player_one_score
+end
 
-# def player_two_score
-#   @player_two_score
-# end
+def player_two_score
+  @player_two_score
+end
 
 
-# # writer method
-# def player_one_score=(score)
-#   @player_one_score += score
-# end
+# writer method
+def player_one_score=(score)
+  @player_one_score += score
+end
 
-# def player_two_score=(score)
-#   @player_two_score += score
-# end
+def player_two_score=(score)
+  @player_two_score += score
+end
 
-# def wins?(player_one_decision, player_two_decision)
-# # hand = ['rock', 'paper', 'scissors']
-# # @player_one = player_one
-# # @player_two = player_two
-# # player_one_score = 0
-# # player_two_score = 0
+def wins?(player_one_decision, player_two_decision)
+# hand = ['rock', 'paper', 'scissors']
+# @player_one = player_one
+# @player_two = player_two
+# player_one_score = 0
+# player_two_score = 0
 
-# if player_one_decision == 'rock' && player_two_decision == 'scissors'
-#   @player_one_score = 1
-#   end
-# end
-# end
-# end
+if player_one_decision == 'rock' && player_two_decision == 'scissors' || player_one_decision == 'scissors' && player_two_decision == 'paper' || player_one_decision == 'paper' && player_two_decision == 'rock'
+  @player_one_score = 1
+  end
+end
+end
+
 
 
 
--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -3,63 +3,66 @@ require('rock_paper_scissors')
 
 # -----simplified version-----
 
-describe(RPS) do
-describe('#wins?') do
-  it("returns true if rock is the object and scissors is the argument") do
-  game = RPS.new()
-  expect(game.wins?("rock", "scissors")).to(eq(true))
-  end
+# describe(RPS) do
+# describe('#wins?') do
+#   it("returns true if rock is the object and scissors is the argument") do
+#   game = RPS.new()
+#   expect(game.wins?("rock", "scissors")).to(eq(true))
+#   end
 
-    it("returns true if scissors is the object and paper is the argument") do
-      game2 = RPS.new()
-      expect(game2.wins?("scissors", "paper")).to(eq(true))
-    end
+#     it("returns true if scissors is the object and paper is the argument") do
+#       game2 = RPS.new()
+#       expect(game2.wins?("scissors", "paper")).to(eq(true))
+#     end
 
-    it("returns true if paper is the object and rock is the argument") do
-      game3 = RPS.new()
-      expect(game3.wins?("paper", "rock")).to(eq(true))
-  end
+#     it("returns true if paper is the object and rock is the argument") do
+#       game3 = RPS.new()
+#       expect(game3.wins?("paper", "rock")).to(eq(true))
+#   end
 
-  it("returns false if scissor is the object and rock is the argument") do
-      game4 = RPS.new()
-      expect(game4.wins?("scissors", "rock")).to(eq(false))
-    end
+#   it("returns false if scissor is the object and rock is the argument") do
+#       game4 = RPS.new()
+#       expect(game4.wins?("scissors", "rock")).to(eq(false))
+#     end
 
-    it("returns false if paper is the object and scissors is the argument") do
-      game5 = RPS.new()
-      expect(game5.wins?("paper", "scissors")).to(eq(false))
-    end
+#     it("returns false if paper is the object and scissors is the argument") do
+#       game5 = RPS.new()
+#       expect(game5.wins?("paper", "scissors")).to(eq(false))
+#     end
 
-    it("returns false if rock is the object and paper is the argument") do
-      game6 = RPS.new()
-      expect(game6.wins?("rock", "paper")).to(eq(false))
-    end
+#     it("returns false if rock is the object and paper is the argument") do
+#       game6 = RPS.new()
+#       expect(game6.wins?("rock", "paper")).to(eq(false))
+#     end
 
-    it("returns nil if the object is equal too the argument") do
-      game7 = RPS.new()
-      game8 = RPS.new()
-      game9 = RPS.new()
-      expect(game7.wins?("rock", "rock")).to(eq(nil))
-      expect(game8.wins?("scissors", "scissors")).to(eq(nil))
-      expect(game9.wins?("paper", "paper")).to(eq(nil))
-    end
-end
-end
+#     it("returns nil if the object is equal too the argument") do
+#       game7 = RPS.new()
+#       game8 = RPS.new()
+#       game9 = RPS.new()
+#       expect(game7.wins?("rock", "rock")).to(eq(nil))
+#       expect(game8.wins?("scissors", "scissors")).to(eq(nil))
+#       expect(game9.wins?("paper", "paper")).to(eq(nil))
+#     end
+#   end
+# end
 
 
 
 
 # -----score version-----
 
-# describe(RPS) do
-#   describe('#wins?') do
-#     it("returns true if rock is the object and scissors is the argument") do
-#       game = RPS.new()
-#       game.wins?("rock", "scissors")
-#       expect(game.player_one_score).to(eq(1))
-#     end
-#   end
-# end
+describe(RPS) do
+  describe('#wins?') do
+    it("returns true if rock is the object and scissors is the argument") do
+      game = RPS.new()
+      game.wins?("rock", "scissors")
+      game.wins?("scissors", "paper")
+      game.wins?("rock", "scissors")
+      game.wins?("paper", "rock")
+      expect(game.player_one_score).to(eq(1))
+    end
+  end
+end
 
 
 
